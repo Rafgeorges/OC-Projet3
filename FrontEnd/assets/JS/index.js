0e39de461235621c058e3d6bcdbea283d17f7417
RafGeorges <raf.georges@gmail.com>
2023-08-30 17:14:22 +0200

    Galerie JS - appel fonction et creation boucle travaux

--- FrontEnd/assets/JS/index.js
+++ FrontEnd/assets/JS/index.js
@@ -1,18 +1,23 @@
 
+
+
 //MENU //////////////////////////////////////////
 const header = document.querySelector('header')
 const nav = document.createElement("nav")
 const ul = document.createElement("ul")
+
 //MENU - projets
 const liProjets = document.createElement("li")
 const lienProjet = document.createElement("a")
 lienProjet.innerText= "projets"
 lienProjet.href = ""
+
 //MENU - contact
 const liContact = document.createElement("li")
 const lienContact = document.createElement('a')
 lienContact.innerText = "contact"
 lienContact.href = ''
+
 //MENU - lien login
 const liLogin = document.createElement("li")
 const lienLogin = document.createElement("a")
@@ -39,3 +44,49 @@ liLogin.appendChild(lienLogin)
 ul.appendChild(liInsta)
 liInsta.appendChild(lienInsta)
 lienInsta.appendChild(ImgInsta)
+
+
+
+
+
+
+//GALLERY ////////////////////////////////////////////
+//GALLERY -  Appel de l'API
+const requeteTravaux = await fetch("http://localhost:5678/api/works")
+const travaux = await requeteTravaux.json()
+
+//GALLERY - Déclaration des éléments
+const gallery = document.createElement("div")
+gallery.classList.add('gallery')
+const portfolio = document.querySelector('#portfolio')
+portfolio.appendChild(gallery)
+
+//GALLERY - Fonction creation des travaux
+function genererTravaux(travaux){
+    try{
+        for(let i=0; i<travaux.length; i++){
+                const figure = document.createElement("figure")
+                const imageTravaux = document.createElement("img")
+                const titresTravaux = document.createElement("figcaption")
+
+                imageTravaux.src = travaux[i].imageUrl
+                imageTravaux.setAttribute("alt",travaux[i].title)
+                titresTravaux.innerText = travaux[i].title
+                figure.setAttribute('id','travaux[i].id')
+
+                gallery.appendChild(figure)
+                figure.appendChild(imageTravaux)
+                figure.appendChild(titresTravaux)
+            }
+        }
+    catch(error){
+        console.log(error)
+    }
+}
+
+
+genererTravaux(travaux)
+
+
+//GALLERY - Parenting
+
